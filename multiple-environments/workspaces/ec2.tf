599cea80b291095d811d3e20b13ce12ee35014c8
chand1997 <chanduchallahalli@gmail.com>
2025-02-03 13:38:07 +0530

    demonstrating workspace feature in terraform which helps multiple envs

---
+++ multiple-environments/workspaces/ec2.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = lookup(var.instance_type, terraform.workspace)
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "Workspace-demo-${terraform.workspace}"
+  }
+}
+
+
+
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_${terraform.workspace}"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  
+
+
+  tags = {
+    Name = "allow_tls_${terraform.workspace}"
+  }
+}
